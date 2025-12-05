0673097ce54d9de8aae4b59929113d74bf3ac168
Niffer-dev <142007953+Niffer-dev@users.noreply.github.com>
2025-12-05 11:06:19 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Learnreact from './component/Learnreact'
+
+const App = () => {
+  return (
+    <div>
+      <Learnreact/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Learnreact.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Learnreact = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username === "") {
+            alert('Username is required!');
+            return;
+        }
+
+        if (!password.match(/^(?=.*[0-9])(?=.*[@])/)) {
+            alert("password must include @ and a number");
+            return;
+        }
+
+        alert("Form submitted successfully!");
+        console.log("username:", username);
+        console.log("password:", password);
+
+    }
+
+
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <h2>Username</h2>
+            <input type="text" name='' placeholder='Enter username' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <h2>Password</h2>
+            <input type="password" name="" id="" placeholder='Enter your password' value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button type='submit'>Login</button>
+        </form>
+    </div>
+  )
+}
+
+export default Learnreact
